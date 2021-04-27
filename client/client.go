bdd074d719e6e9f6bcd5c331534f8bad48241c28
hellojukay <licong@qianxin.com>
2021-04-27 10:33:27 +0800

    finish auth

--- client/client.go
+++ client/client.go
@@ -23,15 +23,25 @@ func New(api string, token string) Client {
 }
 
 func (c Client) Ping() bool {
-	url, err := url.Parse(c.url)
+	url, err := url.Parse(c.url + "/projects")
 	if err != nil {
 		log.Fatalf("api scheme format error '%s' %s", c.url, err)
 	}
 	req := http.Request{
 		Method: "GET",
 		URL:    url,
+		Header: http.Header{},
 	}
-	req.Header.Add("PRIVATE-TOKEN", c.token)
-	c.Do(&req)
-	return true
+	req.Header.Set("PRIVATE-TOKEN", c.token)
+	res, err := c.Do(&req)
+	if err != nil {
+		log.Printf("request %s failed, %s", c.url, err)
+		return false
+	}
+	if res.StatusCode == 200 {
+		return true
+	} else {
+		log.Printf("%s %d", c.url, res.StatusCode)
+	}
+	return false
 }
--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/hellojukay/gitlab/client"
 )
 
 func main() {
-	client := client.New("", "")
+	var api = os.Getenv("API")
+	client := client.New(api, os.Getenv("TOKEN"))
 	if !client.Ping() {
 		log.Fatalln("can not connect to gitlab")
+	} else {
+		log.Printf("auth %s success", api)
 	}
 }
